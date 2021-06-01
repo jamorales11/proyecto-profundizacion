6e098df49ea778c36168003427b2b22bc8a1e7d5
Julio Morales <ja.morales11@uniandes.edu.co>
2021-06-01 18:56:07 -0500

    Visualizacion

---
+++ Assets/Scripts/Controlador.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlador : MonoBehaviour
+{
+    AnimationCurve anim1;
+    AnimationCurve anim2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animacion();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( this.transform.position.y > 27){
+            this.GetComponent<FogEffect>().enabled = false;
+            this.GetComponent<UnderwaterEffect>().enabled = false;
+            this.GetComponent<Camera>().farClipPlane = 900;
+        }
+        else{
+            this.GetComponent<FogEffect>().enabled = true;
+            this.GetComponent<UnderwaterEffect>().enabled = true;
+            this.GetComponent<Camera>().farClipPlane = 180;
+        }
+
+        this.transform.position = new Vector3(transform.position.x, anim1.Evaluate(Time.time), transform.position.y);
+        this.transform.rotation =  Quaternion.Euler(anim2.Evaluate(Time.time), 180f, transform.rotation.z);
+    }
+
+
+
+    void animacion()
+    {
+        Keyframe[] bajar = new Keyframe[3];
+        bajar[0] = new Keyframe(0f, 60f);
+        bajar[1] = new Keyframe(5f, 60f);
+        bajar[2] = new Keyframe(10f, 20f);
+
+        Keyframe[] rotar = new Keyframe[4];
+        rotar[0] = new Keyframe(0f, 15f);
+        rotar[1] = new Keyframe(7.5f, 15f);
+        rotar[2] = new Keyframe(10f, 0f);
+        rotar[3] = new Keyframe(12f, -15f);
+        
+
+        anim1 = new AnimationCurve(bajar);
+        anim2 = new AnimationCurve(rotar);
+
+    }
+}
---
+++ Assets/Scripts/UnderwaterEffect.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class UnderwaterEffect : MonoBehaviour
+{
+
+    public Material _mat;
+
+    [Range(0.001f, 0.1f)]
+    public float _pixelOffset;
+
+    [Range(0.1f, 20f)]
+    public float _noiseScale;
+
+    [Range(0.1f, 20f)]
+    public float _noiseFrequency;
+
+    [Range(0.1f, 30f)]
+    public float _noiseSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _mat.SetFloat("_NoiseFrequency",  _noiseFrequency);
+        _mat.SetFloat("_NoiseSpeed",  _noiseSpeed);
+        _mat.SetFloat("_NoiseScale",  _noiseScale);
+        _mat.SetFloat("_PixelOffset",  _pixelOffset);
+    }
+
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest) {
+        Graphics.Blit(src, dest, _mat);
+    }
+}
